55f7014109b6fd4f27e32bc1c0efee2821e7f54b
MilleSimon <isak.logenius@gmail.com>
2026-03-05 01:20:20 +0100

    Main refactor

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.Console;
+
+public class Main {
+    private static final int X = 800;
+    private static final int Y = 800;
+
+    // The delay (ms) corresponds to 20 updates a sec (hz)
+    private static final int delay = 50;
+
+    // The timer is started with a anonymous listener (see below) that executes the UI tick
+    // each step between delays
+    static Timer timer;
+    static CarController cc;
+    static CarFrame frame;
+
+    static void main(String[] args) {
+        Dimension dimension = new Dimension(X, Y);
+        cc = new CarController(dimension);
+        frame = new CarFrame("CarSim 2.0", dimension, cc);
+
+        timer = new Timer(delay, _ -> {
+            cc.simulationTick();
+            frame.updateFrame();
+        });
+        timer.setRepeats(true);
+        timer.start();
+
+        // Add the cars and set their starting positions below each other by 100 units (compensated for image height of the previous car)
+        cc.cars.add(new Volvo240());
+        cc.cars.add(new Saab95());
+        cc.cars.getLast().setPosition(new Position(0,160));
+        cc.cars.add(new Scania());
+        cc.cars.getLast().setPosition(new Position(0,320));
+
+        cc.workshops.add(new VolvoWorkshop(new Position(300, 300), new Rotation(0)));
+        cc.workshops.getFirst().openDoors();
+    }
+}
